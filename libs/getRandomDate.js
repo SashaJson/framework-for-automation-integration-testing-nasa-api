4326152e47a08946dff49a9c2171cd15368c842e
SashaJson <sashamiller666@gmail.com>
2020-10-26 22:19:06 +0200

    changed func random date

--- libs/getRandomDate.js
+++ libs/getRandomDate.js
@@ -3,15 +3,14 @@
 const moment = require("moment");
 const generationInteger = require('./getRandomInteger');
 
-module.exports ={
+module.exports = () => {
 
-    randomValidDate: () => {
-        const dateFormat = "YYYY-MM-DD";
+    const DATE_FORMAT = "YYYY-MM-DD";
 
-        const validYear =  generationInteger.getRndInteger(1996, 2020);
-        const validMouth = generationInteger.getRndInteger(0, 11);
-        const validDate = generationInteger.getRndInteger(1, 31);
+    const validYear = generationInteger(1996, 2020);
+    const validMouth = generationInteger(0, 11);
+    const validDate = generationInteger(1, 31);
 
-        return moment(`${validYear}-${validMouth}-${validDate}`).format(dateFormat)
-    }
+    return moment(`${validYear}-${validMouth}-${validDate}`).format(DATE_FORMAT)
+
 }
