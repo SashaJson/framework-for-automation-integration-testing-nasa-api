e16aebbc8be87ad7a9e67b666baee29b2830a2f6
SashaJson <sashamiller666@gmail.com>
2021-01-17 22:04:57 +0200

    changed in config

--- config/defaults.js
+++ config/defaults.js
@@ -1,15 +1,32 @@
-"use strict";
+'use strict';
 
 module.exports = {
 
     //---------------Configurations for API-NASA---------------
-    API_KEY: "vidkL09R9ihkBAcesBGRgQtwiJkSfXQcToZi1aov",
-    URL_APOD: "https://api.nasa.gov/planetary/apod",
-    rateLimit: '2000',
+
+    API_KEY: 'vidkL09R9ihkBAcesBGRgQtwiJkSfXQcToZi1aov',
+    URL_APOD: 'https://api.nasa.gov/planetary/apod',
+    RATE_LIMIT: '2000',
+
 
     //---------------Global configurations for Jest---------------
-    jestTimeout: 40 * 1000,
+
+    JEST_TIMEOUT: 40 * 1000,
+
 
     //---------------Parameters for Headers---------------
-    contentType: 'application/json'
+
+    CONTENT_TYPE: 'application/json',
+
+
+    //---------------Format Dates-------------------------
+
+    DATE_FORMAT: 'YYYY-MM-DD',
+    INVALID_DATE: '1919-03-06',
+    MINIMAL_DATE: '1995-06-16',
+    MIN_DATE_MINUS_1: '1995-06-15',
+
+    //--------------Type Images--------------------------
+
+    TYPE_IMAGE: 'image'
 }
