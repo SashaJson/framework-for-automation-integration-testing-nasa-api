b027dc8c51b9f845ddf23e867cf949086553276b
SashaJson <sashamiller666@gmail.com>
2021-11-11 00:48:15 +0200

    updated response models

--- json-schemas/response-models/apiKeyMissing.js
+++ json-schemas/response-models/apiKeyMissing.js
@@ -1,32 +1,28 @@
 'use strict';
 
-module.exports = () => {
-
-    return {
-        "type": "object",
-        "required": [
-            "error"
-        ],
-        "properties": {
-            "error": {
-                "type": "object",
-                "required": [
-                    "code",
-                    "message"
-                ],
-                "additionalProperties": false,
-                "properties": {
-                    "code": {
-                        "type": "string",
-                        "const": "API_KEY_MISSING",
-                    },
-                    "message": {
-                        "type": "string",
-                        "const": "No api_key was supplied. Get one at https://api.nasa.gov:443"
-                    }
+module.exports = () => ({
+    "type": "object",
+    "required": [
+        "error"
+    ],
+    "properties": {
+        "error": {
+            "type": "object",
+            "required": [
+                "code",
+                "message"
+            ],
+            "additionalProperties": false,
+            "properties": {
+                "code": {
+                    "type": "string",
+                    "const": "API_KEY_MISSING",
+                },
+                "message": {
+                    "type": "string",
+                    "const": "No api_key was supplied. Get one at https://api.nasa.gov:443"
                 }
             }
         }
-    };
-
-}
+    }
+})
--- json-schemas/response-models/defaultResult.js
+++ json-schemas/response-models/defaultResult.js
@@ -1,24 +1,20 @@
 'use strict';
 
-module.exports = () => {
-
-    return {
-        "type": "object",
-        "allOf": [
-            {
-                "$ref": "apod.json#"
-            },
-            {
-                "required": [
-                    "date",
-                    "explanation",
-                    "media_type",
-                    "service_version",
-                    "title",
-                    "url"
-                ]
-            }
-        ]
-    };
-
-}
+module.exports = () => ({
+    "type": "object",
+    "allOf": [
+        {
+            "$ref": "apod.json#"
+        },
+        {
+            "required": [
+                "date",
+                "explanation",
+                "media_type",
+                "service_version",
+                "title",
+                "url"
+            ]
+        }
+    ]
+})
