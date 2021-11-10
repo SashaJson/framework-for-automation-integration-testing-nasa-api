290b901fb2eb8f89b11da1ba52c6ae88c6f523b1
SashaJson <sashamiller666@gmail.com>
2021-11-11 00:48:44 +0200

    added static data

--- static-data/defaults.js
+++ static-data/defaults.js
@@ -4,19 +4,8 @@ module.exports = {
 
     //---------------Configurations for API-NASA---------------
 
-    API_KEY: 'vidkL09R9ihkBAcesBGRgQtwiJkSfXQcToZi1aov',
     URL_APOD: 'https://api.nasa.gov/planetary/apod',
     RATE_LIMIT: '2000',
-
-
-    //---------------Global configurations for Jest---------------
-
-    JEST_TIMEOUT: 40 * 1000,
-
-
-    //---------------Parameters for Headers---------------
-
-    CONTENT_TYPE: 'application/json',
 
 
     //---------------Format Dates-------------------------
@@ -24,10 +13,10 @@ module.exports = {
     DATE_FORMAT: 'YYYY-MM-DD',
     INVALID_DATE: '1919-03-06',
     MINIMAL_DATE: '1995-06-16',
-    MIN_DATE_MINUS_1: '1995-06-15',
+
 
     //--------------Type Images--------------------------
 
-    TYPE_IMAGE: 'image',
+    TYPE_IMAGE: 'image'
 
 }
