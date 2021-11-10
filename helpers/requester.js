532cb32a969eb62df54f325924e91ea0327af730
SashaJson <sashamiller666@gmail.com>
2021-11-11 01:23:57 +0200

    updated requester.js

--- helpers/requester.js
+++ helpers/requester.js
@@ -1,40 +1,42 @@
 'use strict';
 
+const inspect = require('util').inspect;
 const fetch = require('node-fetch');
-const {getCurlRequest} = require('./utils');
 
-module.exports = async (url, method = 'GET', data = null) => {
+const { BASE_URL } = require('../static-data/defaults');
 
-    getCurlRequest(url, method, data);
+async function get(path) {
 
-    try {
+    const METHOD = 'GET';
 
-        let body;
-        const headers = {};
+    console.log(inspect(`curl --request ${ METHOD } '${ BASE_URL }${ path }'`, { showHidden: false, depth: null }));
 
-        if (data) {
-            headers['Content-Type'] = 'application/json';
-            body = JSON.stringify(data)
-        }
+    const requestOptions = { method: METHOD };
 
-        const response = await fetch(url, {
-            method,
-            headers,
-            body
-        });
+    const response = await fetch(BASE_URL + path, requestOptions);
 
-        console.log('Data when make request: ' + Date());
+    return handleResponse(response);
 
-        for (let [key, value] of response.headers) {
-            console.log(`Headers: ${key} = ${value}`);
-        }
+}
 
-        console.log(`HTTP-cod response: ${response.status}`);
 
-        return await response;
+function handleResponse(response) {
 
-    } catch (error) {
-        console.error('Error when make request', error.message);
+    console.log('Date when make request: ' + Date());
+
+    for (let [key, value] of response.headers) {
+        console.log(`Headers: ${ key } = ${ value }`);
+    }
+
+    console.log(`HTTP-cod response: ${ response.status }`);
+
+    if (!response.ok) {
+        console.error(`Server return HTTP-cod: ${ response.status }`);
+        return response.text();
+    } else {
+        return response.json();
     }
 
 }
+
+module.exports = { get }
