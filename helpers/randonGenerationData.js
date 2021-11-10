4658eb74a838b4d4493fc7554c982a8176c5df1e
SashaJson <sashamiller666@gmail.com>
2021-11-11 01:23:31 +0200

    added randomeGenerationData.js to dir helpers

---
+++ helpers/randonGenerationData.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const moment = require('moment');
+const { DATE_FORMAT } = require('../static-data/defaults');
+
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+}
+
+function getRandomDate() {
+
+    const validYear = getRandomInteger(1996, 2020);
+    const validMouth = getRandomInteger(0, 11);
+    const validDate = getRandomInteger(1, 31);
+
+    return moment(`${ validYear }-${ validMouth }-${ validDate }`).format(DATE_FORMAT);
+}
