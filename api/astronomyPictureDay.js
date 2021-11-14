559a1f05f6a34ab3bb10ee40aa2d5aaa9ad868dd
SashaJson <sashamiller666@gmail.com>
2021-11-14 20:28:23 +0200

    updated api/apod

--- api/astronomyPictureDay.js
+++ api/astronomyPictureDay.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const { APOD_URL } = require('../static-data/defaults');
+
+const { verificationResponseWithJsonModel } = require('../helpers/assertResponse');
 const r = require('../helpers/requester');
 
 module.exports = {
@@ -8,6 +10,10 @@ module.exports = {
     async getAstronomyPictureDay(expectResponseError = false) {
 
         const responseBody = await r.get(`${APOD_URL}?api_key=${process.env.API_KEY}`);
+
+        verificationResponseWithJsonModel(responseBody, expectResponseError);
+
+        return responseBody;
 
     }
 
