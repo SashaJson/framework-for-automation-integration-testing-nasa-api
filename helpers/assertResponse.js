22234645966be67e5a60a786b9a37cc5d3bcc3e5
SashaJson <sashamiller666@gmail.com>
2022-01-15 22:54:47 +0200

    updated helpers/assertResponse

--- helpers/assertResponse.js
+++ helpers/assertResponse.js
@@ -1,15 +1,20 @@
 'use strict';
 
+const invalidParameterJsonModel = require('../json-schemas/response-models/invalidParameter');
 const defaultJsonModel = require('../json-schemas/response-models/defaultResult');
-const errorJsonModel = require('../json-schemas/response-models/apiKeyMissing');
+const apiKeyMissingJsonModel = require('../json-schemas/response-models/apiKeyMissing');
 
 const validateJsonSchema = require('../helpers/validation-json-schemas');
 
-function verificationResponseWithJsonModel(responseBody, error) {
-    if (!error) {
-        validateJsonSchema(responseBody, defaultJsonModel());
+function verificationResponseWithJsonModel(expectError, responseBody, jsonSchema = defaultJsonModel()) {
+    if (expectError.includes(false)) {
+        validateJsonSchema(responseBody, jsonSchema);
     } else {
-        validateJsonSchema(responseBody, errorJsonModel());
+        ({
+            'invalidParam': () => validateJsonSchema(responseBody, invalidParameterJsonModel()),
+            'apiKeyMiss': () => validateJsonSchema(responseBody, apiKeyMissingJsonModel())
+        })[expectError[1]]();
+
     }
 }
 
